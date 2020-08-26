4199ea61f08d2cca60c907ecb13cc0c1cbe3d8a4
shirtiny <shirtiny@gmail.com>
2020-08-26 11:29:45 +0800

    createStore的第二个参数说明

--- src/demo-js/store.js
+++ src/demo-js/store.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
 
-// 使用redux的createStore函数 传入自定的reducer 以创建state
+// 使用redux的createStore函数 传入自定的reducer 以创建state 第二个可选参数是state的初始值
 const store = createStore(reducer);
 
 export default store;
