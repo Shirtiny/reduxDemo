5edbc0f870327697a298f6e79e576ce185c842e2
shirtiny <shirtiny@gmail.com>
2020-08-26 12:06:32 +0800

    redux开发工具配置

--- src/demo-js/store.js
+++ src/demo-js/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./reducer";
 
 // 使用redux的createStore函数 传入自定的reducer 以创建state 第二个可选参数是state的初始值
-const store = createStore(reducer);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 
 export default store;
